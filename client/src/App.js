3ee22e8df48f81dd8c692b503fcdf90ea29a36ca
Gary Magill <123419905+GaryMagill22@users.noreply.github.com>
2023-09-17 15:49:44 -0600

    newsletter emaiil added

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 // import ForeBearText from './Components/ForeBearText';
 // import HeaderSection from './Components/HeaderSection';
 import LandingPage from './Pages/LandingPage';
+import NewsLetterSignup from './Components/NewsLetterSignup';
 
 
 
@@ -14,7 +15,7 @@ function App() {
       {/* <HeaderSection /> */}
       {/* <ForeBearText /> */}
       <LandingPage />
-
+      {/* <NewsLetterSignup /> */}
       {/* <Routes>
 
         <Route path="/" element={<ForeBearText />} />
