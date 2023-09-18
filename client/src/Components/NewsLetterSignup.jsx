474a064c8a8938531e0d778edc758be1f4b7cb41
Gary Magill <123419905+GaryMagill22@users.noreply.github.com>
2023-09-17 20:49:16 -0600

    layout looks good - newsletter signup good

--- client/src/Components/NewsLetterSignup.jsx
+++ client/src/Components/NewsLetterSignup.jsx
@@ -28,7 +28,7 @@ function NewsLetterSignup() {
         axios.post('http://localhost:8000/Signup', { email })
             .then(response => {
                 console.log(response.data.message);
-                // Maybe show a success message to the user
+
             })
             .catch(error => {
                 console.error("Error registering email:", error);
@@ -37,14 +37,15 @@ function NewsLetterSignup() {
     }
 
     return (
+
         <div className="py-16 sm:py-24">
             <div className="mx-auto max-w-7.5xl sm:px-6 lg:px-10">
                 <div className="relative isolate overflow-hidden bg-gray-900 px-8 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
                     <h2 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                        Get notified when we’re launching.
+                        Get notified when we’re launching!
                     </h2>
                     <p className="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-300">
-                        Reprehenderit ad esse et non officia in nulla. Id proident tempor incididunt nostrud nulla et culpa.
+                        Sign up to receive updates on our launch and be the first to know when we’re live.
                     </p>
                     <form className="mx-auto mt-10 flex max-w-md gap-x-4" onSubmit={handleSubmit}>
                         <label htmlFor="email-address" className="sr-only">
